f7a5f3792e6e6920a7979a11d3492e0bf5b85613
Ben Churchill <benchurchill@ntlworld.com>
2013-07-06 03:00:30 +0100

    Setup Workspace

---
+++ src/com/minercore/creeperwood/benchcubed/stuntajai/SuperBaseBattle/SuperBaseBattle.java
@@ -0,0 +1,15 @@
+package com.minercore.creeperwood.benchcubed.stuntajai.SuperBaseBattle;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ben
+ * Date: 06/07/13
+ * Time: 01:06
+ * To change this template use File | Settings | File Templates.
+ */
+public class SuperBaseBattle extends JavaPlugin {
+
+
+}
