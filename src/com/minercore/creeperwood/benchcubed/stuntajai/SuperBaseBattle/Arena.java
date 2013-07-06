3f64089b9a63f23e9de7bec81fd4fafe0bec1a05
Ben Churchill <benchurchill@ntlworld.com>
2013-07-06 17:52:01 +0100

    General additions

--- src/com/minercore/creeperwood/benchcubed/stuntajai/SuperBaseBattle/Arena.java
+++ src/com/minercore/creeperwood/benchcubed/stuntajai/SuperBaseBattle/Arena.java
@@ -1,10 +1,20 @@
 package com.minercore.creeperwood.benchcubed.stuntajai.SuperBaseBattle;
 
+import com.minercore.creeperwood.benchcubed.stuntajai.SuperBaseBattle.Utils.SuperBaseBattlePlayer;
+import com.minercore.creeperwood.benchcubed.stuntajai.SuperBaseBattle.enums.Team;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+
+
 /**
  * User: Ben
  * Date: 06/07/13
  * Time: 14:09
  */
+
 public class Arena {
 
 	private int arenaId;
@@ -12,16 +22,49 @@ public class Arena {
 	private int maxX;
 	private int minZ;
 	private int maxZ;
+	private Location redSpawn;
+	private Location blueSpawn;
 
-	public Arena(int minX, int maxX, int minZ, int maxZ, int arenaId) {
+	ArrayList<String> playersInGame = new ArrayList<String>();
+
+	public Arena(int minX, int maxX, int minZ, int maxZ, Location redSpawn, Location blueSpawn, int arenaId) {
 		this.minX = minX;
 		this.maxX = maxX;
 		this.minZ = minZ;
 		this.maxZ = maxZ;
+		this.arenaId = arenaId;
+		this.redSpawn = redSpawn;
+		this.blueSpawn = blueSpawn;
+	}
+
+	public boolean addPlayer(Player player, Team team) {
+		if (team == null || player == null || ((redSpawn == null && team == Team.RED) || (blueSpawn == null && team == Team.BLUE)))
+			return false;
+		SuperBaseBattlePlayer superBaseBattlePlayer = new SuperBaseBattlePlayer(player, team);
+		return superBaseBattlePlayer.teleportToArena(this) && superBaseBattlePlayer.giveArmour(team) && playersInGame.add(player.getName());
+	}
+
+	public boolean removePlayer(Player player) {
+		return playersInGame.remove(player.getName());
+	}
+
+	public boolean teleportAllToSpawn() {
+		boolean failure = false;
+		for (String s : playersInGame)
+			if (!Locations.teleportToSpawn(Bukkit.getPlayer(s))) failure = true;
+		return !failure;
 	}
 
 	public int getMinX() {
 		return minX;
+	}
+
+	public Location getRedSpawn() {
+		return redSpawn;
+	}
+
+	public Location getBlueSpawn() {
+		return blueSpawn;
 	}
 
 	public int getMaxX() {
--- src/com/minercore/creeperwood/benchcubed/stuntajai/SuperBaseBattle/SuperBaseBattle.java
+++ src/com/minercore/creeperwood/benchcubed/stuntajai/SuperBaseBattle/SuperBaseBattle.java
@@ -1,27 +1,66 @@
 package com.minercore.creeperwood.benchcubed.stuntajai.SuperBaseBattle;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
- * Created with IntelliJ IDEA.
  * User: Ben
  * Date: 06/07/13
  * Time: 01:06
- * To change this template use File | Settings | File Templates.
  */
 public class SuperBaseBattle extends JavaPlugin {
 
-    public static void main(String[] args){
-        // Not Used
-    }
 
-    public void onEnable() {
-        this.getLogger().info(getDescription().getName() + " Has Been Enabled!");
-    }
+	public static ArrayList<Arena> arenaList = new ArrayList<Arena>();
+	public static World spawnWorld;
 
-    public void onDisable() {
-        this.getLogger().info(getDescription().getName() + " Has Been Disabled!");
-    }
+	public static void main(String[] args) {
+		// Not Used
+	}
+
+	public void onEnable() {
+		this.getLogger().info(getDescription().getName() + " Has Been Enabled!");
+		if (!new File(getDataFolder(), "Reset.file").exists()) try {
+			new File(getDataFolder(), "Reset.file").createNewFile();
+			getConfig().set("SBB World", "SuperBaseBattle");
+			getConfig().set("Spawn World", "Spawn");
+			getConfig().set("Arenas.0.minX", "0");
+			getConfig().set("Arenas.0.maxX", "40");
+			getConfig().set("Arenas.0.minZ", "0");
+			getConfig().set("Arenas.0.maxZ", "40");
+			getConfig().set("Arenas.0.RedSpawn", "10,64,20");
+			getConfig().set("Arenas.0.BlueSpawn", "30,64,20");
+			saveConfig();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		defineVariables();
+	}
+
+	public void onDisable() {
+		this.getLogger().info(getDescription().getName() + " Has Been Disabled!");
+	}
+
+	private void defineVariables() {
+		spawnWorld = Bukkit.getWorld(getConfig().getString("SBB World"));
+		for (String s : getConfig().getConfigurationSection("Arenas").getKeys(false)) {
+			try {
+				Integer.valueOf(s);
+			} catch (Exception e) {
+				continue;
+			}
+			String[] redSpawn = getConfig().getString("Arenas." + s + ".RedSpawn").split(",");
+			String[] blueSpawn = getConfig().getString("Arenas." + s + ".BlueSpawn").split(",");
+			int redSpawnX = Integer.valueOf(redSpawn[0]), redSpawnY = Integer.valueOf(redSpawn[1]), redSpawnZ = Integer.valueOf(redSpawn[2]);
+			int blueSpawnX = Integer.valueOf(blueSpawn[0]), blueSpawnY = Integer.valueOf(blueSpawn[1]), blueSpawnZ = Integer.valueOf(blueSpawn[2]);
+
+			arenaList.add(new Arena(getConfig().getInt("Arenas." + s + ".minX"), getConfig().getInt("Arenas." + s + ".maxX"), getConfig().getInt("Arenas." + s + ".minZ"), getConfig().getInt("Arenas." + s + ".maxZ"), new Location(Bukkit.getWorld(getConfig().getString("SBB World")), redSpawnX, redSpawnY, redSpawnZ), new Location(Bukkit.getWorld(getConfig().getString("SBB World")), blueSpawnX, blueSpawnY, blueSpawnZ), Integer.valueOf(s)));
+		}
+	}
 }
